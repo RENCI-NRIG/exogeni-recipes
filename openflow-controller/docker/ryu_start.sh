3a0e9091b0543ec2e19c7aa957012af8d46765e8
Mert Cevik <mcevik@renci.org>
2020-02-25 12:11:17 -0500

    update ryu_start for app directory

--- openflow-controller/docker/ryu_start.sh
+++ openflow-controller/docker/ryu_start.sh
@@ -5,8 +5,8 @@ RYU_REST_APP="ofctl_rest.py"
 RYU_PID_FILE="/var/run/ryu/ryu-manager.pid"
 RYU_LOG_FILE="/var/log/ryu/ryu-manager.log"
 RYU_CONFIG_DIR="/opt/ryu/etc"
-RYU_APP="/opt/simple_switch_13_custom_chameleon.py"
-RYU_REST="/opt/ofctl_rest.py"
+RYU_APP="/opt/ryu_app/simple_switch_13_custom_chameleon.py"
+RYU_REST="/opt/ryu_app/ofctl_rest.py"
 OFP_TCP_LISTEN_PORT="6653"
 
 /usr/bin/ryu-manager --pid-file ${RYU_PID_FILE} --ofp-tcp-listen-port ${OFP_TCP_LISTEN_PORT} --log-file ${RYU_LOG_FILE} --app-lists ${RYU_REST} ${RYU_APP}
